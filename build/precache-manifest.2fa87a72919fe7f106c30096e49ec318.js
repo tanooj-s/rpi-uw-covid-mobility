70fab140b6dd00551f7f06cf17ce3fe5c70eea54
Tanooj Shah <tanooj@Tanoojs-MacBook-Air.local>
2020-08-17 17:20:03 -0700

    Untracked files issue resolved to fix .gitignore

---
+++ build/precache-manifest.2fa87a72919fe7f106c30096e49ec318.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bb6169e4eda86f5652a0f6cc13717ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d24f55bc21ed5c0cf48f",
+    "url": "/static/css/main.8b05e947.chunk.css"
+  },
+  {
+    "revision": "18158ec1517bd11d2da5",
+    "url": "/static/js/2.36cb9c77.chunk.js"
+  },
+  {
+    "revision": "853eeb9143990f2ec5dfb3e4b5ef074d",
+    "url": "/static/js/2.36cb9c77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d24f55bc21ed5c0cf48f",
+    "url": "/static/js/main.309d70c4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
